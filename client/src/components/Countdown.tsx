544e91ee6eb8440d4d2b244cc0ad8ad55dc6025a
AkuinAshura <samuel.louis.mason@gmail.com>
2026-01-09 12:16:30 +1300

    feat: added skull animation during boss fights, based on approximate difficulty

--- client/src/components/Countdown.tsx
+++ client/src/components/Countdown.tsx
@@ -12,6 +12,7 @@ function Countdown() {
   >();
   const [now, setNow] = useState(dayjs());
   const [bosses, setBosses] = useState<Boss[] | null>(null);
+  const [bossFight, setBossFight] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,7 +26,7 @@ function Countdown() {
 
   // For testing hypothetical boss timers.
   const future = useMemo(() => {
-    return now.add(0, "day").add(0, "hour").add(0, "minute").add(8, "second");
+    return now.add(0, "day").add(0, "hour").add(0, "minute").add(4, "second");
   }, []);
 
   useEffect(() => {
@@ -69,29 +70,62 @@ function Countdown() {
   // Safety?
   useEffect(() => {
     if (nextBossDateTime !== undefined) {
-      const titleCard = document.getElementById("titleCard");
-      // const title = document.getElementById("title");
       if (nextBossDateTime?.unix() <= now.unix()) {
-        titleCard?.classList.add(styles.bossTitleCard);
-        // title?.setAttribute("style", "opacity: 0;");
-      } else {
-        titleCard?.classList.remove(styles.bossTitleCard);
-        // title?.setAttribute("style", "opacity: 1;");
+        setBossFight(true);
       }
     }
   }, [getFormattedCountdown]);
+
+  useEffect(() => {
+    if (bossFight === true) {
+      const titleCard = document.getElementById("titleCard");
+      titleCard?.classList.add(styles.bossTitleCard);
+      placeBossSkulls();
+    }
+  }, [bossFight]);
+
+  const sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  async function placeBossSkulls() {
+    let skullCount = 0;
+    const skullContainer = document.getElementById("bossSkullContainer");
+    nextBosses?.map((boss) => {
+      if (boss.note?.toLowerCase().includes("event")) {
+        skullCount += 1;
+      } else if (boss.note?.toLowerCase().includes("half")) {
+        skullCount += 2;
+      } else if (boss.note?.toLowerCase().includes("longer")) {
+        skullCount += 5;
+      } else {
+        skullCount += 3;
+      }
+    });
+    for (let i = 0; i < skullCount; i++) {
+      const skull = document.createElement("div");
+      skull.classList.add(styles.bossSkull);
+      skullContainer?.appendChild(skull);
+      await sleep(500);
+    }
+    await sleep(1500); // Allow time for the skulls to move before rendering text.
+    document.getElementById("bossText")?.setAttribute("style", "opacity: 1");
+  }
 
   return (
     <>
       <div id="titleCard" className={styles.titleCard}>
         {nextBosses !== undefined ? (
-          <div id="title" className={styles.title}>
-            {nextBosses?.map((boss, i) => (
-              <div key={boss.name} style={{ display: "flex" }}>
-                {i !== 0 && <hr className={styles.verticalRule} />}
-                <div className={styles.bossName}>{boss.name}</div>
-              </div>
-            ))}
+          <div id="bossSkullContainer" className={styles.bossSkullContainer}>
+            <div id="title" className={styles.title}>
+              {bossFight !== true &&
+                nextBosses?.map((boss, i) => (
+                  <div key={boss.name} style={{ display: "flex" }}>
+                    {i !== 0 && <hr className={styles.verticalRule} />}
+                    <div className={styles.bossName}>{boss.name}</div>
+                  </div>
+                ))}
+            </div>
           </div>
         ) : (
           <div>No Upcoming Bosses</div>
@@ -99,11 +133,15 @@ function Countdown() {
       </div>
       <div className={styles.timer}>
         {nextBossDateTime !== undefined &&
-          nextBossDateTime?.unix() > now.unix() && (
+          nextBossDateTime.unix() > now.unix() && (
             <div>{getFormattedCountdown()}</div>
           )}
         {nextBossDateTime !== undefined &&
-          nextBossDateTime.unix() <= now.unix() && <div>Boss Time!</div>}
+          nextBossDateTime.unix() <= now.unix() && (
+            <div id="bossText" style={{ opacity: 0 }}>
+              Boss Fight!
+            </div>
+          )}
       </div>
     </>
   );
