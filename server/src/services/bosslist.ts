06ffd7f9cb491dee0a57f8d1da8eb50e57020d02
Biscuitmunch <biscuitmunch0@gmail.com>
2026-01-08 22:29:55 +1300

    feat: conditional create db
    Co-authored-by: NachoToast <NachoToast@users.noreply.github.com>

--- server/src/services/bosslist.ts
+++ server/src/services/bosslist.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from "express";
+import { DatabaseSync, StatementSync } from "node:sqlite";
+import path, { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 
 interface Boss {
   name: string;
@@ -7,20 +11,21 @@ interface Boss {
   type: string;
 }
 
-import { DatabaseSync } from "node:sqlite";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { existsSync, writeFileSync } from "node:fs";
-
 let dbPath: string;
 
-if (process.argv.includes('--IN_DOCKER')) {
-  dbPath = path.join(import.meta.dirname, "..", "..", "docker_db", "terraria.db");
+if (process.argv.includes("--IN_DOCKER")) {
+  dbPath = path.join(
+    import.meta.dirname,
+    "..",
+    "..",
+    "docker_db",
+    "terraria.db"
+  );
 
   if (!existsSync(dbPath)) {
     // we can't do a create outside of Docker because if the user is running in
     // dev (which has hot reloading) we get an infinite reload loop
-    writeFileSync(dbPath, '', 'utf-8');
+    writeFileSync(dbPath, "", "utf-8");
   }
 } else {
   const __filename = fileURLToPath(import.meta.url);
@@ -30,8 +35,32 @@ if (process.argv.includes('--IN_DOCKER')) {
 
 const db = new DatabaseSync(dbPath);
 
+const rawSql = readFileSync(
+  join(import.meta.dirname, "..", "db", "tables", "bosslist.sql"),
+  "utf-8"
+);
+
+const splitInd = rawSql.indexOf(");");
+
+const createTable = rawSql.slice(0, splitInd + 2).trim();
+const insertInto = rawSql.slice(splitInd + 2).trim();
+
+if (!createTable || !insertInto) {
+  throw new Error("Missing SQL File");
+}
+
+let query: StatementSync;
+
+try {
+  query = db.prepare("SELECT * FROM bosslist");
+} catch (error) {
+  console.log("No DB detected, creating one :)");
+  db.exec(createTable);
+  db.exec(insertInto);
+  query = db.prepare("SELECT * FROM bosslist");
+}
+
 export function bosslist(req: Request, res: Response) {
-  const query = db.prepare("SELECT * FROM bosslist where type = Boss");
   const bosses: Boss[] = query.all() as unknown as Boss[]; // Idk how to make this type safe...
   res.json(bosses);
 }
