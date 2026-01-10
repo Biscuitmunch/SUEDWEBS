b9357b22f60233ea842935ca7dcbfe2d97fbd32a
AkuinAshura <samuel.louis.mason@gmail.com>
2026-01-11 08:27:28 +1300

    feat: colour change, animation speedup, refactor, fix, chore
    feat: halo changes colour and speeds up at boss time.
    refactor: setInterval and setTimeout used for timing, component logic.
    fix: two skulls no longer placed at once, reloading and saving doesn't result in unexpected behaviour.
    chore: various other file changes.

--- client/src/components/Countdown.tsx
+++ client/src/components/Countdown.tsx
@@ -5,137 +5,187 @@ import type { Boss } from "./BossList.tsx";
 import { BASE_URL } from "../constants";
 
 function Countdown() {
-  const [nextBossDateTime, setNextBossDateTime] = useState<
-    dayjs.Dayjs | undefined
-  >();
-  const [now, setNow] = useState(dayjs());
-  const [bosses, setBosses] = useState<Boss[] | null>(null);
+    const [nextBossDateTime, setNextBossDateTime] = useState<
+        dayjs.Dayjs | undefined
+    >();
+    const [now, setNow] = useState(dayjs());
+    const [bosses, setBosses] = useState<Boss[] | undefined>(undefined); // Expects to receive [] for zero bosses.
+    const [skullCounter, setSkullCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setNow(dayjs());
-    }, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNow(dayjs());
+        }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
-  useEffect(() => {
-    fetch(`${BASE_URL}/nextbosstime`)
-      .then((response) => response.json())
-      .then((timestamp) => setNextBossDateTime(dayjs.unix(timestamp)));
-    fetch(`${BASE_URL}/bosslist`)
-      .then((response) => response.json())
-      .then(setBosses);
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch(`${BASE_URL}/nextbosstime`)
+                .then((response) => response.json())
+                .then((timestamp) => setNextBossDateTime(dayjs.unix(timestamp)))
+                .catch(() => setNextBossDateTime(undefined));
+            fetch(`${BASE_URL}/bosslist`)
+                .then((response) => response.json())
+                .then(setBosses)
+                .catch(() => setBosses(undefined));
+        };
+        fetchData();
+    }, []);
 
-  const nextBosses = useMemo(() => {
-    return bosses?.filter((boss) => boss.type === "current");
-  }, [bosses]);
-
-  const bossFight = useMemo(() => {
-    return (
-      nextBossDateTime !== undefined && now.unix() >= nextBossDateTime.unix()
-    );
-  }, [now, nextBossDateTime]);
-
-  function getFormattedCountdown() {
-    if (nextBossDateTime === undefined) return "";
-    const timeUntilBoss = dayjs.duration(nextBossDateTime.diff(now));
-
-    const timeUnits = [
-      { duration: timeUntilBoss.days(), denomination: "day" },
-      { duration: timeUntilBoss.hours(), denomination: "hour" },
-      { duration: timeUntilBoss.minutes(), denomination: "minute" },
-      { duration: timeUntilBoss.seconds(), denomination: "second" },
-    ];
-
-    const leadingDenominationIndex = timeUnits.findIndex(
-      (unit) => unit.duration > 0
-    );
-    const formattedCountdown = timeUnits
-      .slice(leadingDenominationIndex)
-      .map((unit) => {
-        return `${unit.duration} ${
-          unit.duration === 1 ? unit.denomination : unit.denomination + "s"
-        }`;
-      })
-      .join(", ");
-    return formattedCountdown;
-  }
-
-  const sleep = (ms: number) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
-
-  useEffect(() => {
-    if (bossFight === true) {
-      const placeBossSkulls = async () => {
-        let skullCount = 0;
-        const skullContainer = document.getElementById("bossSkullContainer");
-        nextBosses?.map((boss) => {
-          if (boss.note?.toLowerCase().includes("event")) {
-            skullCount += 1;
-          } else if (boss.note?.toLowerCase().includes("half")) {
-            skullCount += 2;
-          } else if (boss.note?.toLowerCase().includes("longer")) {
-            skullCount += 5;
-          } else {
-            skullCount += 3;
-          }
-        });
-        for (let i = 0; i < skullCount; i++) {
-          const skull = document.createElement("div");
-          skull.classList.add(styles.bossSkull);
-          skullContainer?.appendChild(skull);
-          await sleep(500);
+    const nextBosses = useMemo(() => {
+        if (bosses !== undefined && bosses.length === 0) {
+            return null;
         }
-        await sleep(1500); // Allow time for the skulls to move before rendering text.
-        document
-          .getElementById("bossText")
-          ?.setAttribute("style", "opacity: 1");
-      };
+        return bosses?.filter((boss) => boss.type === "current");
+    }, [bosses]);
 
-      const titleCard = document.getElementById("titleCard");
-      titleCard?.classList.add(styles.bossTitleCard);
-      placeBossSkulls();
+    const bossFight = useMemo(() => {
+        return (
+            nextBossDateTime !== undefined &&
+            now.unix() >= nextBossDateTime.unix()
+        );
+    }, [now, nextBossDateTime]);
+
+    function getFormattedCountdown() {
+        if (nextBossDateTime === undefined) return "";
+        const timeUntilBoss = dayjs.duration(nextBossDateTime.diff(now));
+
+        const timeUnits = [
+            { duration: timeUntilBoss.days(), denomination: "day" },
+            { duration: timeUntilBoss.hours(), denomination: "hour" },
+            { duration: timeUntilBoss.minutes(), denomination: "minute" },
+            { duration: timeUntilBoss.seconds(), denomination: "second" },
+        ];
+
+        const leadingDenominationIndex = timeUnits.findIndex(
+            (unit) => unit.duration > 0
+        );
+        const formattedCountdown = timeUnits
+            .slice(leadingDenominationIndex)
+            .map((unit) => {
+                return `${unit.duration} ${
+                    unit.duration === 1
+                        ? unit.denomination
+                        : unit.denomination + "s"
+                }`;
+            })
+            .join(", ");
+        return formattedCountdown;
     }
-  }, [bossFight, nextBosses]);
 
-  return (
-    <>
-      <div id="titleCard" className={styles.titleCard}>
-        {nextBosses !== undefined ? (
-          <div id="bossSkullContainer" className={styles.bossSkullContainer}>
-            <div id="title" className={styles.title}>
-              {bossFight !== true &&
-                nextBosses?.map((boss, i) => (
-                  <div key={boss.name} style={{ display: "flex" }}>
-                    {i !== 0 && <hr className={styles.verticalRule} />}
-                    <div className={styles.bossName}>{boss.name}</div>
-                  </div>
-                ))}
+    const skullCount = useMemo(() => {
+        let skullCount = 0;
+        nextBosses?.map((boss) => {
+            if (boss.note?.toLowerCase().includes("event")) {
+                skullCount += 1;
+            } else if (boss.note?.toLowerCase().includes("half")) {
+                skullCount += 2;
+            } else if (boss.note?.toLowerCase().includes("longer")) {
+                skullCount += 5;
+            } else {
+                skullCount += 3;
+            }
+        });
+        return skullCount;
+    }, [nextBosses]);
+
+    useEffect(() => {
+        if (nextBosses !== null && bossFight) {
+            const bossHalo = document.getElementById("bossHalo");
+            if (bossHalo) {
+                bossHalo.style.setProperty("--halo-colour", "var(--ctp-red)");
+            }
+
+            const placeBossSkull = () => {
+                const bossTitleCard = document.getElementById("bossTitleCard");
+                const skull = document.createElement("div");
+                skull.classList.add(styles.bossSkull);
+                bossTitleCard?.appendChild(skull);
+            };
+
+            const interval = setInterval(() => {
+                setSkullCounter((skullCounter: number) => {
+                    const nextCount = skullCounter + 1;
+                    if (bossHalo && nextCount > 1) {
+                        bossHalo.classList.remove("spin");
+                        void bossHalo.offsetWidth; // Supposedly this is for reflow, not sure if needed.
+                        bossHalo.style.setProperty(
+                            "--animation-duration",
+                            12 / (nextCount + 1) + "s"
+                        );
+                        bossHalo.classList.add("spin");
+                    }
+                    if (nextCount > skullCount) {
+                        clearInterval(interval);
+                        return skullCount;
+                    }
+                    return nextCount;
+                });
+                if (skullCounter < skullCount) placeBossSkull(); // it's beautiful :.>
+            }, 1200);
+
+            const timeout = setTimeout(() => {
+                document
+                    .getElementById("bossText")
+                    ?.setAttribute("style", "opacity: 1");
+            }, 1500 + 500 * skullCount); // lmao
+            return () => {
+                clearInterval(interval);
+                clearTimeout(timeout);
+            };
+        }
+    }, [nextBosses, bossFight, skullCount, skullCounter]);
+
+    return (
+        <>
+            <div id="bossHalo" className={styles.bossHalo}>
+                <div id="bossTitleCard" className={styles.bossTitleCard}>
+                    <div id="title" className={styles.title}>
+                        {nextBosses !== null ? (
+                            <>
+                                {bossFight !== true &&
+                                    nextBosses?.map((boss, i) => (
+                                        <div
+                                            key={boss.name}
+                                            style={{ display: "flex" }}
+                                        >
+                                            {i !== 0 && (
+                                                <hr
+                                                    className={
+                                                        styles.verticalRule
+                                                    }
+                                                />
+                                            )}
+                                            <div className={styles.bossName}>
+                                                {boss.name}
+                                            </div>
+                                        </div>
+                                    ))}
+                            </>
+                        ) : (
+                            <div>No Upcoming Bosses</div>
+                        )}
+                    </div>
+                </div>
             </div>
-          </div>
-        ) : (
-          <div>No Upcoming Bosses</div>
-        )}
-      </div>
-      <div className={styles.timer}>
-        {nextBossDateTime !== undefined &&
-          nextBossDateTime.unix() > now.unix() && (
-            <div>{getFormattedCountdown()}</div>
-          )}
-        {nextBossDateTime !== undefined &&
-          nextBossDateTime.unix() <= now.unix() && (
-            <div id="bossText" style={{ opacity: 0 }}>
-              Boss Fight!
+            <div className={styles.timer}>
+                {nextBossDateTime !== undefined &&
+                    nextBossDateTime.unix() > now.unix() && (
+                        <div>{getFormattedCountdown()}</div>
+                    )}
+                {nextBossDateTime !== undefined &&
+                    nextBossDateTime.unix() <= now.unix() && (
+                        <div id="bossText" style={{ opacity: 0 }}>
+                            Boss Fight!
+                        </div>
+                    )}
             </div>
-          )}
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Countdown;
