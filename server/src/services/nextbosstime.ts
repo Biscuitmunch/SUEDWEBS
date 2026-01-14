271407268607450a40a0f8d6a05b3fb6d7a48f8c
Biscuitmunch <biscuitmunch0@gmail.com>
2026-01-15 02:14:50 +1300

    chore: update next boss time

--- server/src/services/nextbosstime.ts
+++ server/src/services/nextbosstime.ts
@@ -1,13 +1,13 @@
-import dayjs from "dayjs";
-import { Request, Response } from "express";
+import dayjs from 'dayjs';
+import { Request, Response } from 'express';
 
-import timezone from "dayjs/plugin/timezone.js";
-import utc from "dayjs/plugin/utc.js";
+import timezone from 'dayjs/plugin/timezone.js';
+import utc from 'dayjs/plugin/utc.js';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-const bossTimestamp = dayjs.tz("2026-01-12 03:46:30", "Pacific/Auckland").unix();
+const bossTimestamp = dayjs.tz('2026-01-16 19:00:00', 'Pacific/Auckland').unix();
 
 export function nextbosstime(req: Request, res: Response) {
   res.json(bossTimestamp);
