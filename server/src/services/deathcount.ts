c1dc9572b77176b7aefc18f3db45619834bb6750
Biscuitmunch <biscuitmunch0@gmail.com>
2026-01-16 01:53:51 +1300

    feat: create table for deathcount

--- server/src/services/deathcount.ts
+++ server/src/services/deathcount.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
-import { DatabaseSync } from 'node:sqlite';
+import { DatabaseSync, StatementSync } from 'node:sqlite';
 import { existsSync, readFileSync, writeFileSync } from 'node:fs';
-import path from 'node:path';
+import path, { join } from 'node:path';
+
+interface PlayerData {
+  playerName: string;
+  playerDeaths: number;
+}
 
 const inDocker = process.argv.includes('--IN_DOCKER');
 
@@ -19,18 +24,37 @@ if (!existsSync(dbPath)) {
 
 const db = new DatabaseSync(dbPath);
 
-interface PlayerData {
-  playerName: string;
-  playerDeaths: number;
+const rawSql = readFileSync(
+  join(import.meta.dirname, '..', '..', 'db', 'tables', 'deathcount.sql'),
+  'utf-8'
+);
+
+const splitInd = rawSql.indexOf(');');
+
+const createTable = rawSql.slice(0, splitInd + 2).trim();
+
+if (!createTable) {
+  throw new Error('Missing SQL File');
+}
+
+let query: StatementSync;
+
+try {
+  query = db.prepare(`
+    INSERT INTO deathcount (name, deaths) VALUES (?, ?)
+    ON CONFLICT(name) DO UPDATE SET deaths=(?);
+  `);
+} catch (error) {
+  console.log('No DB detected, creating one :)');
+  db.exec(createTable);
+  query = db.prepare(`
+    INSERT INTO deathcount (name, deaths) VALUES (?, ?)
+    ON CONFLICT(name) DO UPDATE SET deaths=(?);
+  `);
 }
 
 export function deathcount(req: Request, res: Response) {
   const data: PlayerData = req.body;
-
-  const query = db.prepare(`
-    INSERT INTO deathcount (name, deaths) VALUES (?, ?)
-    ON CONFLICT(name) DO UPDATE SET deaths=(?);
-  `);
 
   query.run(data.playerName, data.playerDeaths, data.playerDeaths);
 
