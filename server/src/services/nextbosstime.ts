61ce01d73bc713c9b2213cd62521dba6f6e44a63
Biscuitmunch <biscuitmunch0@gmail.com>
2026-01-15 18:04:48 +1300

    fix: proper day

--- server/src/services/nextbosstime.ts
+++ server/src/services/nextbosstime.ts
@@ -7,7 +7,7 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-const bossTimestamp = dayjs.tz('2026-01-16 19:00:00', 'Pacific/Auckland').unix();
+const bossTimestamp = dayjs.tz('2026-01-15 19:00:00', 'Pacific/Auckland').unix();
 
 export function nextbosstime(req: Request, res: Response) {
   res.json(bossTimestamp);
