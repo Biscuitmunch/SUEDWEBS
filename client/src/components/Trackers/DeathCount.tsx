d96cb4f0359efc86965818162bec1d28e9af744e
AkuinAshura <samuel.louis.mason@gmail.com>
2026-01-17 10:57:29 +1300

    feat: button to sort players by death count ascending/descending, refactor: moved titles css into FontStyles
    There is currently a bug where players are not ordered correctly on page load.

--- client/src/components/Trackers/DeathCount.tsx
+++ client/src/components/Trackers/DeathCount.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { BASE_URL } from '../../constants';
 import styles from './DeathCount.module.css';
+import titles from '../FontStyles.module.css';
+import type { MouseEvent } from 'react';
 
 interface Player {
   id: number;
@@ -8,8 +10,15 @@ interface Player {
   deaths: number;
 }
 
+const ascDesc = {
+  asc: true,
+  desc: false,
+} as const;
+
 function DeathCount() {
   const [players, setPlayers] = useState<Player[]>([]);
+  const [order, setOrder] = useState<typeof ascDesc.asc | typeof ascDesc.desc>(ascDesc.asc);
+  const playerOrder = useRef<Player[]>([]);
 
   useEffect(() => {
     fetch(`${BASE_URL}/deathcount`)
@@ -17,10 +26,40 @@ function DeathCount() {
       .then((data) => setPlayers(data));
   }, []);
 
+  function orderByDeaths() {
+    setPlayers(playerOrder.current);
+    setOrder(!order);
+    console.log(order);
+  }
+
+  useEffect(() => {
+    if (order) {
+      const sortedPlayers = players.sort((a, b) => a.deaths - b.deaths);
+      playerOrder.current = sortedPlayers;
+    } else {
+      const sortedPlayers = players.sort((a, b) => b.deaths - a.deaths);
+      playerOrder.current = sortedPlayers;
+    }
+  }, [order, players]);
+
+  // useEffect(() => {
+  //   orderByDeaths();
+  // }, []);
+
+  const sortByDeaths = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    orderByDeaths();
+  };
+
   return (
     <div className={styles.gridContainer}>
-      <h2>Player Death Counts</h2>
+      <div className={titles.title2}>Player Info</div>
       <div className={styles.grid}>
+        <div className={styles.gridHeaders}>Player Name</div>
+        <div className={styles.gridHeaders}>
+          Death Count
+          <button id="button" onClick={sortByDeaths} className={styles.ascDescButton} />
+        </div>
         {players.map((player) => (
           <div key={player.id} className={styles.playerItem}>
             <div className={styles.playerName}>{player.name}</div>
