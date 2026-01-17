8d1673b83bc67f039bc2451a08e3dcf0b83e961a
Biscuitmunch <biscuitmunch0@gmail.com>
2026-01-17 22:41:29 +1300

    fix: import order

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone.js';
+import utc from 'dayjs/plugin/utc.js';
 import { info } from './services/info.js';
 import { getBossList } from './services/bosslist.js';
 import { nextbosstime } from './services/nextbosstime.js';
 import { getDeathCount, putDeathCount } from './services/deathcount.js';
 import { setBossKills } from './services/bosskills.js';
-import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone.js';
-import utc from 'dayjs/plugin/utc.js';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
