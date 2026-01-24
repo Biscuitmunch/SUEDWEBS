44886d1b0ffa757dcb6272be15b479ead0f3fcbf
AkuinAshura <samuel.louis.mason@gmail.com>
2026-01-25 04:33:19 +1300

    style: added calendar and buttons (not functional)
    Added one week calendar with legend, user select button, tooltip and submit (all buttons and calendar not yet functional)

--- client/src/components/NextBossPoll.tsx
+++ client/src/components/NextBossPoll.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import styles from './NextBossPoll.module.css';
 
 interface Props {
@@ -7,17 +7,28 @@ interface Props {
 }
 
 function NextBossPoll({ bossName, visibility }: Props) {
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
   // const [selectedUser, setSelectedUser] = useState(0);
   // const [selectedTimes, setSelectedTimes] = useState(0);
 
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   const times = [12, ...[...Array(11).keys()].slice(1)];
 
+  function toggleDropdown() {
+    setDropdownVisibility(!dropdownVisibility);
+  }
+
   return (
     <>
       {visibility && (
         <div className={styles.pollArea}>
-          <div className={styles.title}>{bossName} Poll</div>
+          <div className={styles.titleBox}>
+            <div className={styles.title}>{bossName} Poll</div>
+          </div>
+          <div className={styles.legend}>
+            <span className={styles.legendToday}>■ Today's Date</span>
+            <span className={styles.legendAvailable}>■ Available Dates</span>
+          </div>
           <div className={styles.dayTimeSelectionGrid}>
             {days.map((day: string, index: number) => (
               <div key={index}>
@@ -32,7 +43,14 @@ function NextBossPoll({ bossName, visibility }: Props) {
               </div>
             ))}
           </div>
-          <div className={styles.buttonBar}></div>
+          <div className={styles.buttonBar}>
+            <div className={styles.userDropdownButton}>Select User ⮟</div>
+            <div className={styles.submissionArea}>
+              <div className={styles.questionMark}>?</div>
+              <span className={styles.toolTip}>&nbsp;Tooltip</span>
+              <div className={styles.submitButton}>Submit</div>
+            </div>
+          </div>
         </div>
       )}
     </>
