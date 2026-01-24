cb15853497d0710746cfc4d1d289b0b2542d5452
AkuinAshura <samuel.louis.mason@gmail.com>
2026-01-25 08:43:52 +1300

    style: colour coded headers, added tooltip styling, button highlights

--- client/src/components/NextBossPoll.tsx
+++ client/src/components/NextBossPoll.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import styles from './NextBossPoll.module.css';
 
 interface Props {
@@ -7,16 +7,19 @@ interface Props {
 }
 
 function NextBossPoll({ bossName, visibility }: Props) {
-  const [dropdownVisibility, setDropdownVisibility] = useState(false);
+  // const [dropdownVisibility, setDropdownVisibility] = useState(false);
   // const [selectedUser, setSelectedUser] = useState(0);
   // const [selectedTimes, setSelectedTimes] = useState(0);
 
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   const times = [12, ...[...Array(11).keys()].slice(1)];
 
-  function toggleDropdown() {
-    setDropdownVisibility(!dropdownVisibility);
-  }
+  const today = 'Sunday';
+  const available = ['Monday', 'Saturday', 'Sunday'];
+
+  // function toggleDropdown() {
+  //   setDropdownVisibility(!dropdownVisibility);
+  // }
 
   return (
     <>
@@ -32,7 +35,21 @@ function NextBossPoll({ bossName, visibility }: Props) {
           <div className={styles.dayTimeSelectionGrid}>
             {days.map((day: string, index: number) => (
               <div key={index}>
-                <div className={styles.dayGridHeaders}>{day[0]}</div>
+                {available.includes(day) && day != today && (
+                  <div className={styles.availableHeader}>&thinsp;{day[0]}</div>
+                )}
+                {!available.includes(day) && day != today && (
+                  <div className={styles.unavailableHeader}>&thinsp;{day[0]}</div>
+                )}
+                {available.includes(day) && day === today && (
+                  <div className={styles.availableHeader}>
+                    &thinsp;{day[0]}
+                    <div className={styles.availableTodayHeader} />
+                  </div>
+                )}
+                {!available.includes(day) && day === today && (
+                  <div className={styles.todayHeader}>&thinsp;{day[0]}</div>
+                )}
                 <div className={styles.dayGridItem}>
                   {times.map((time: number, index: number) => (
                     <div key={index} className={styles.timeGridItem}>
@@ -44,10 +61,14 @@ function NextBossPoll({ bossName, visibility }: Props) {
             ))}
           </div>
           <div className={styles.buttonBar}>
-            <div className={styles.userDropdownButton}>Select User ⮟</div>
+            <div className={styles.userDropdown}>
+              <div className={styles.userDropdownButton}>Select User ⮟</div>
+            </div>
             <div className={styles.submissionArea}>
               <div className={styles.questionMark}>?</div>
-              <span className={styles.toolTip}>&nbsp;Tooltip</span>
+              <div className={styles.toolTip}>
+                If you don't see your name here, please speak to the server owners.
+              </div>
               <div className={styles.submitButton}>Submit</div>
             </div>
           </div>
